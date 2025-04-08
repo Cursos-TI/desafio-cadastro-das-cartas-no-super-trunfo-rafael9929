49263d2eb6e7cba12b23a205f649c58b6ce4f146
rafael9929 <damsceno.rafa2@gmail.com>
2025-04-08 22:58:26 +0000

    dasafio nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,15 +6,35 @@ int main(){
       char estado[20];
       char codigo[3];
       char cidade[20];
-      float populacao = 1;
+      unsigned populacao = 1;
       float area = 2;
       float pib = 3;
       int turismo =4;
       float densidade =5;
       float percapta =6;
+      float suprpoder =7;
+      char estado2[20];
+      char codigo2[3];
+      char cidade2[20];
+      unsigned populacao2 = 1;
+      float area2 = 2;
+      float pib2 = 3;
+      int turismo2 =4;
+      float densidade2 =5;
+      float percapta2 =6;
+      float suprpoder2 =7;
+      float comparacaop;
+      float comparacaoa;
+      float comparacaopi;
+      float comparacaot;
+      float comparacaod;
+      float comparacaope;
+      float comparacaos;
 
+              //entrada de dados 
 
              //cadastro de carta 01
+
 
               printf("CARTA - 01 \n");
   
@@ -28,7 +48,7 @@ int main(){
          scanf("%s", &cidade);
 
          printf("população: \n");
-         scanf("%f", &populacao);
+         scanf("%u", &populacao);
 
          printf("Área em km²: \n");
          scanf("%f", &area);
@@ -39,9 +59,14 @@ int main(){
 
         printf("numero de pontos turísticos: \n");
         scanf("%d", &turismo);
+ 
+          //regras de cauculos 
 
     densidade =(populacao / area);
     percapta = (pib / populacao);
+    suprpoder =(populacao + area + pib + turismo + percapta - densidade);
+ 
+          //saida de dados  
 
  printf("CARTA - 01 \n");
  printf("estado: %s \n", estado);
@@ -49,63 +74,94 @@ int main(){
  printf("codigo da carta: %s \n", codigo);
  printf("cidade: %s \n", cidade);
 
- printf("populaçao: %f \n ", populacao);
- printf("Área em m²: %f \n", area);
+ printf("populaçao: %.2u \n ", populacao);
+ printf("Área em m²: %.2f \n", area);
 
- printf("PIB: %f \n " , pib);
- printf("numero de pontos turísticos: %d \n",turismo);
+ printf("PIB: %.2f \n " , pib);
+ printf("numero de pontos turísticos: %.2d \n",turismo);
 
- printf("densidade populacional:%f \n" ,densidade);
-printf("pib per capta;%f \n" , percapta);
-
+ printf("densidade populacional:%.2f \n" ,densidade);
+printf("pib per capta;%.2f \n" , percapta);
  
+printf("super poder:%.2f \n" , suprpoder);
+ 
+                 //entrada de dados 
+
                  //cadastro carta 02
 
              printf("CARTA - 02 \n");
 
          printf("estado: \n");
-         scanf("%s", &estado);  
+         scanf("%s", &estado2);  
 
          printf("codigo da carta: \n");
-         scanf("%s", &codigo);
+         scanf("%s", &codigo2);
 
          printf("cidade: \n");
-         scanf("%s", &cidade);
+         scanf("%s", &cidade2);
 
          printf("população: \n");
-         scanf("%f", &populacao);
+         scanf("%u", &populacao2);
 
          printf("Área em km²: \n");
-         scanf("%f", &area);
+         scanf("%f", &area2);
 
         printf("PIB: \n");
-        scanf("%f", &pib);
+        scanf("%f", &pib2);
 
          printf("numero de pontos turísticos: \n");
-         scanf("%d", &turismo);
+         scanf("%d", &turismo2);
 
-     densidade =(populacao / area);
-     percapta = (pib / populacao);
-        
+                //regras de cauculos 
+
+     densidade2 =(populacao2 / area2);
+     percapta2 = (pib2 / populacao2);
+     suprpoder2 =(populacao2 + area2 + pib2 + turismo2 + percapta2 - densidade2);
+
+                //saida de dados  
+
              printf("CARTA - 02 \n");
 
- printf("estado: %s \n", estado);
- printf("codigo da carta: %s \n", codigo);
+ printf("estado: %s \n", estado2);
+ printf("codigo da carta: %s \n", codigo2);
 
 
-printf("cidade: %s \n", cidade);
-printf("populaçao: %f \n ", populacao);
+printf("cidade: %s \n", cidade2);
+printf("populaçao: %.2u \n ", populacao2);
    
-printf("Área em m²: %f \n", area);
-printf("PIB: %f \n " , pib);
+printf("Área em m²: %.2f \n", area2);
+printf("PIB:%.2f \n " , pib2);
 
-printf("numero de pontos turísticos: %d \n",turismo);
+printf("numero de pontos turísticos: %.2d \n",turismo2);
 
-printf("densidade populacional:%f \n" ,densidade);
-printf("pib per capta;%f \n" , percapta);
+printf("densidade populacional:%.2f \n" ,densidade2);
+printf("pib per capta:%.2f \n" , percapta2);
+
+printf("super poder:%.2f \n" , suprpoder2);
 
 printf("CADASTRADO ! \n");
 
+          //comparaçao das cartas 
+comparacaop =(populacao > populacao2);
+comparacaoa = (area > area2);
+comparacaopi = (pib > pib2);
+comparacaot = (turismo > turismo2);
+comparacaod =(densidade > densidade2);
+comparacaope = (percapta > percapta2);
+comparacaos = (suprpoder > suprpoder2);
+         
+
+              //impreçao das comparaçao das cartas 
+
+          printf("COMPARAÇAO DE CARTAS\n");
+          
+          printf("populaçao:%.2f \n" , comparacaop);
+          printf("area:%.2f \n" , comparacaoa);
+          printf("pib:%.2f \n" , comparacaopi);
+          printf("pontos turisticos:%.2f \n" , comparacaot);
+          printf("densidade populacional:%.2f \n" , comparacaod);
+          printf("pib per capta:%.2f \n" , comparacaope);
+          printf("super poder:%.2f \n" , comparacaos);
 
 return 0; 
 } 
